3b20abb980e209d80aa009343c527a63cd453c75
Bassem Reda <34349984+BassemReda@users.noreply.github.com>
2018-11-30 13:29:20 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static int numPumps;
+    public static int numClients;
+    public static ArrayList<String> clients = new ArrayList<>();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of Pumps: ");
+        numPumps = sc.nextInt();
+        System.out.print("Enter number of Clients :");
+        numClients = sc.nextInt();
+        System.out.print("Enter clients’ names: ");
+        for (int i = 0; i < numClients; i++)
+            clients.add(sc.next());
+
+        
+    }
+}
